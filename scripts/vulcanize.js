1fd0ac62f9f2c9678c1292e30c882effad797b5c
Oliver Hamlet <oliver.hamlet@gmail.com>
2015-08-27 16:10:16 +0100

    Tidy up Vulcanize script.

--- scripts/vulcanize.js
+++ scripts/vulcanize.js
@@ -6,33 +6,42 @@ var path = require('path');
 var fs = require('fs');
 var helpers = require('./helpers');
 
+function mkdir(dir) {
+    try {
+        fs.mkdirSync(dir);
+    } catch (e) {
+        if (e.code != 'EEXIST') {
+            console.log(e);
+        }
+    }
+}
+
+function buildUI() {
+    if (helpers.isMultiArch(root_path)) {
+        var output_path = path.join(root_path, 'build', '32', 'Release', 'resources', 'ui');
+    } else {
+        var output_path = path.join(root_path, 'build', 'Release', 'resources', 'ui');
+    }
+
+    // Ensure the output directory exists.
+    mkdir(output_path);
+
+    // Vulcanize.
+    child_process.execFileSync('vulcanize.cmd', [
+        '--inline',
+        '--strip',
+        '--config',
+        path.join(root_path, 'scripts', 'vulcanize.config.json'),
+        '-o',
+        path.join(output_path, 'index.html'),
+        path.join(root_path, 'src', 'gui', 'html', 'index.html')
+    ]);
+}
+
 if (process.argv.length < 3) {
     var root_path = '.';
 } else {
     var root_path = process.argv[2];
 }
 
-if (helpers.isMultiArch(root_path)) {
-    var output_path = path.join(root_path, 'build', '32', 'Release', 'resources', 'ui');
-} else {
-    var output_path = path.join(root_path, 'build', 'Release', 'resources', 'ui');
-}
-
-// Makes sure output directory exists first.
-try {
-    fs.mkdirSync(output_path);
-} catch (e) {
-    if (e.code != 'EEXIST') {
-        console.log(e);
-    }
-}
-
-child_process.execFileSync('vulcanize.cmd', [
-    '--inline',
-    '--strip',
-    '--config',
-    path.join(root_path, 'scripts', 'vulcanize.config.json'),
-    '-o',
-    path.join(output_path, 'index.html'),
-    path.join(root_path, 'src', 'gui', 'html', 'index.html')
-]);
+buildUI();
