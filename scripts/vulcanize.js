a201a2b4d3153775a4299f16adc54f0e1c9eedc8
Oliver Hamlet <oliver.hamlet@gmail.com>
2015-08-28 13:27:17 +0100

    Updated loot-message-dialog.
    And removed loot-dialog, it doesn't do enough any more to justify the
    added markup (and data binding was proving tricky to accomplish).

--- scripts/vulcanize.js
+++ scripts/vulcanize.js
@@ -74,6 +74,10 @@ function buildTests() {
             source: path.join('elements', 'test_editable-table.html'),
             dest: path.join('elements', 'test_editable-table.html')
         },
+        {
+            source: path.join('elements', 'test_loot-message-dialog.html'),
+            dest: path.join('elements', 'test_loot-message-dialog.html')
+        },
     ];
 
     tests.forEach(function(test){
